6b31356788fd3e5a12cc7ddb57cc6908d93da879
vaibhavsb0127 <vaibhavsb0127@gmail.com>
2026-07-12 22:04:04 +0530

    Added Chapter 3 and updated Chapter 2 exercises

---
+++ Chapter 2/problemset.py
@@ -0,0 +1,17 @@
+# a= int(input("Enter number 1: "))
+# b= int(input("Enter number 2: "))
+# print("The average of the entered numbers is:", (a + b) / 2)
+
+# c=int(input("Enter number: "))
+# print("The square of the enetered number will be: ",c*c)
+
+name= 'vaibhav'
+nameshort= name[0:9]
+print("The short name is:", nameshort)
+
+name1 = 'vaibhav'
+name1short= name1[-7:-1]
+print("The short name is:", name1short)
+
+print(len(name1))
+print(name1.endswith('end=v'))
---
+++ Chapter 2/variable.py
@@ -0,0 +1,73 @@
+print("hello world!")
+a = 30 # variables = container to store a value
+b = "harry" # keywords = reserved words in python
+c = 71.22 # identifiers = class/function/variable name
+
+c = 71.22 # identifiers = class/function/variable name
+print(" The value of a is:", a)
+print(" The value of b is:", b)
+print(" The value of c is:", c);
+
+d = 5<5
+print (d)
+
+e=False or False or True
+print (e)
+
+print ("the value of true and false is:", True or False)
+print("the value of true and true is:", True or True)
+print ("the value of false and false is:", False or False)
+print ("the value of false and true is:", False or True)
+
+
+
+print ("the value of true or false is:", True and False)
+print("the value of true or true is:", True and True)
+print ("the value of false or false is:", False and False)
+print ("the value of false or true is:", False and True)
+
+print("the value of true xor false is:", True ^ False)
+print("the value of true xor true is:", True ^ True)
+print("the value of false xor false is:", False ^ False)
+print("the value of false xor true is:", False ^ True)
+
+
+print ("the value of not true is:", not True)
+print ("the value of not false is:", not False)
+
+i=98.23
+y=type(i)
+print(y)
+
+o=0b0101
+print(type(o))
+
+h=float(i)
+print(type(h))
+t=type(h)
+t= type(i)
+
+# z=int(input("enter number 1:"))
+# u=int(input("enter number 2:"))
+# print("the sum of two numbers is:", z+u)
+
+
+# problem sets
+
+# j=input("1")
+# k=input("2")
+
+print ("sum",a+b)
+
+q=90
+l=70
+print("remainder=", q%l)
+
+g=23
+v=35
+if g<v:
+    print("g is less than v")
+    
+    print("g is greater than v")
+print("the value of g is:", g)
+
